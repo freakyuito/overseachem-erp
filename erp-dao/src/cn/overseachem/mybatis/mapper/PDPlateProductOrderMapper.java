3c28bc7745e37018dfc11b624eae70c1bdd5e334
freakyuito <freakyuito@gmail.com>
2018-05-23 15:06:17 +0800

    yige
    yige

--- erp-dao/src/cn/overseachem/mybatis/mapper/PDPlateProductOrderMapper.java
+++ erp-dao/src/cn/overseachem/mybatis/mapper/PDPlateProductOrderMapper.java
@@ -1,6 +1,7 @@
 package cn.overseachem.mybatis.mapper;
 
 import cn.overseachem.mybatis.pojo.PDPlateProductOrder;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * Created by Zhihui_Shen on 2018/5/23.
@@ -9,4 +10,5 @@ public interface PDPlateProductOrderMapper {
 
     public void insertOrder(PDPlateProductOrder order);
 
+    public Integer getOrdersAmountInOneYear(@Param("yearBegin") String yearBegin,@Param("yearEnd") String yearEnd);
 }
